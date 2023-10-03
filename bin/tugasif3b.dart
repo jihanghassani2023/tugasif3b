8d03da64ad09ab7b8498333ab4d4616c46236e7d
jihanghassani2023 <jihanghassani_2226250091@mhs.mdp.ac.id>
2023-10-03 13:09:41 +0700

    tugas

--- bin/tugasif3b.dart
+++ bin/tugasif3b.dart
@@ -1,5 +1,29 @@
 import 'package:tugasif3b/tugasif3b.dart' as tugasif3b;
 
-void main(List<String> arguments) {
-  print('Hello world: ${tugasif3b.calculate()}!');
+class PersegiPanjang {
+  int panjang;
+  int lebar;
+
+  PersegiPanjang (this.panjang, this.lebar);
+
+  int hitungLuas (){
+    return panjang * lebar;
+  }
+
+  int hitungKeliling (){
+    return 2*panjang*lebar;
+  }
+}
+
+void main(){
+  int panjang = 5;
+  int lebar = 3;
+
+  PersegiPanjang persegiPanjang =PersegiPanjang(panjang, lebar);
+  
+  int luas = persegiPanjang.hitungLuas();
+  int keliling = persegiPanjang.hitungKeliling();
+
+  print ("Hasil Luas Persegi Panjang : $luas");
+  print("Hasil Keliling Persegi Panjang : $keliling");
 }
